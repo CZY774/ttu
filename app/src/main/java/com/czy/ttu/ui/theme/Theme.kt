246dca4f0b6c442658dcc903ee23e96177c31e93
CZY774 <yogaliuze@gmail.com>
2025-09-30 14:12:29 +0700

    Remove darkColorScheme from Theme.kt
    This commit removes the unused `darkColorScheme` import and its corresponding `DarkColorScheme` definition from the `Theme.kt` file.

--- app/src/main/java/com/czy/ttu/ui/theme/Theme.kt
+++ app/src/main/java/com/czy/ttu/ui/theme/Theme.kt
@@ -3,7 +3,6 @@ package com.czy.ttu.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 
 private val LightColorScheme = lightColorScheme(
