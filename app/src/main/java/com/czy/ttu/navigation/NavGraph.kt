e3334b88028cd2d5f679f5cfcb70c939c8615a7c
CZY774 <yogaliuze@gmail.com>
2025-09-30 20:53:28 +0700

    Add About and Settings screens to NavGraph
    This commit expands the navigation graph by adding routes and composable destinations for the "About" and "Settings" screens.
    
    It also updates the `HomeScreen` to include navigation actions to these new screens.

--- app/src/main/java/com/czy/ttu/navigation/NavGraph.kt
+++ app/src/main/java/com/czy/ttu/navigation/NavGraph.kt
@@ -4,8 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.czy.ttu.ui.screens.AboutScreen
 import com.czy.ttu.ui.screens.CameraScreen
 import com.czy.ttu.ui.screens.HomeScreen
+import com.czy.ttu.ui.screens.SettingsScreen
 
 @Composable
 fun NavGraph() {
@@ -19,12 +21,34 @@ fun NavGraph() {
             HomeScreen(
                 onNavigateToCamera = {
                     navController.navigate(Screen.Camera.route)
+                },
+                onNavigateToAbout = {
+                    navController.navigate(Screen.About.route)
+                },
+                onNavigateToSettings = {
+                    navController.navigate(Screen.Settings.route)
                 }
             )
         }
 
         composable(Screen.Camera.route) {
             CameraScreen(
+                onBack = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composable(Screen.About.route) {
+            AboutScreen(
+                onBack = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composable(Screen.Settings.route) {
+            SettingsScreen(
                 onBack = {
                     navController.popBackStack()
                 }
