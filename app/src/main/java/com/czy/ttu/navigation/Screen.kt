cc123fd90cb60c81eb3e515f841968686be0026e
CZY774 <yogaliuze@gmail.com>
2025-09-30 20:54:04 +0700

    feat: Add About and Settings screens to navigation
    This commit expands the navigation graph by adding routes for the new `About` and `Settings` screens.

--- app/src/main/java/com/czy/ttu/navigation/Screen.kt
+++ app/src/main/java/com/czy/ttu/navigation/Screen.kt
@@ -3,4 +3,6 @@ package com.czy.ttu.navigation
 sealed class Screen(val route: String) {
     object Home : Screen("home")
     object Camera : Screen("camera")
+    object About : Screen("about")
+    object Settings : Screen("settings")
 }
