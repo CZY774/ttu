79fd477f0719e09d7c252db59b1bc3a36faaea0b
CZY774 <yogaliuze@gmail.com>
2025-11-04 09:33:09 +0700

    Refactor: Remove unused import
    Remove unused `FocusMeteringAction` import from `CameraManager.kt`.

--- app/src/main/java/com/czy/ttu/camera/CameraManager.kt
+++ app/src/main/java/com/czy/ttu/camera/CameraManager.kt
@@ -7,7 +7,6 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.core.FocusMeteringAction
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
