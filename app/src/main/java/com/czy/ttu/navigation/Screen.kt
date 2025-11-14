1a5cf770ae907fc0ca0d2b71ea6a1c8bedc0d135
CZY774 <yogaliuze@gmail.com>
2025-11-14 16:45:18 +0700

    Refactor: Remove obsolete Screen sealed class
    The `Screen.kt` file, which defined the sealed class for navigation routes, has been deleted as it is no longer in use.

--- app/src/main/java/com/czy/ttu/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.czy.ttu.navigation
-
-sealed class Screen(val route: String) {
-    object Home : Screen("home")
-    object Camera : Screen("camera")
-    object About : Screen("about")
-    object Settings : Screen("settings")
-}
