68f690f71ce09c27a69468ce9e7170bf419aaf19
CZY774 <yogaliuze@gmail.com>
2025-11-14 16:46:43 +0700

    feat: Add fruit information data
    This commit introduces a new data layer for fruit information.
    
    A `FruitInfo` data class has been created to model the properties of a fruit. Additionally, a `fruit_info.json` file has been added to the assets, containing detailed information such as fun facts, vitamins, and origins for various fruits.

---
+++ app/src/main/java/com/czy/ttu/data/FruitInfo.kt
@@ -0,0 +1,10 @@
+package com.czy.ttu.data
+
+data class FruitInfo(
+    val fun_fact: String,
+    val vitamin: String,
+    val warna: String,
+    val manfaat: String,
+    val asal: String,
+    val fakta_tambahan: String
+)
