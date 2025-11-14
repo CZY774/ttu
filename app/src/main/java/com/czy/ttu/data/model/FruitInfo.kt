e80cfd65ba4292b9291635f1b56737247a0fc308
CZY774 <yogaliuze@gmail.com>
2025-11-14 16:42:51 +0700

    feat: Remove FruitInfo data class
    This commit removes the `FruitInfo.kt` file and its corresponding `FruitInfo` data class, as it is no longer needed.

--- app/src/main/java/com/czy/ttu/data/model/FruitInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.czy.ttu.data.model
-
-data class FruitInfo(
-    val englishName: String,
-    val indonesianName: String,
-    val emoji: String,
-    val funFact: String,
-    val nutritionalBenefits: List<String>
-)
