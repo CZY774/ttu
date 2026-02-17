35239a1b07d63c1d2b5aa0ea101324882b7704d2
CZY774 <yogaliuze@gmail.com>
2026-02-17 16:35:29 +0700

    docs: Add copyright and license header to Color.kt
    A detailed copyright and license header has been added to the top of the `app/src/main/java/com/czy/ttu/ui/theme/Color.kt` file. This header includes the project name "TanyaBuah", copyright years, author names, and intellectual property registration details with the Indonesian Ministry of Law and Human Rights.

--- app/src/main/java/com/czy/ttu/ui/theme/Color.kt
+++ app/src/main/java/com/czy/ttu/ui/theme/Color.kt
@@ -1,3 +1,19 @@
+/**
+ * TanyaBuah - AI Fruit Recognition Application
+ * 
+ * Copyright © 2024-2026 Cornelius Ardhani Yoga Pratama & Pratyaksa Ocsa Nugraha Saian
+ * All Rights Reserved.
+ * 
+ * Protected by Intellectual Property Rights (HKI)
+ * Registration No: 001138316
+ * Directorate General of Intellectual Property (DGIP)
+ * Ministry of Law and Human Rights, Republic of Indonesia
+ * 
+ * This code is proprietary and confidential.
+ * Unauthorized copying, modification, or distribution is prohibited.
+ */
+
+
 package com.czy.ttu.ui.theme
 
 import androidx.compose.ui.graphics.Color
